040017f5a00f27b07b5aceb8b5e660bb86c954f5
zangetsu <dimongames6@gmail.com>
2025-12-27 21:37:01 +0300

    old price in all cards

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -3,12 +3,10 @@ from pathlib import Path
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 from fastapi.middleware.cors import CORSMiddleware
-# from prometheus_fastapi_instrumentator import Instrumentator
 from contextlib import asynccontextmanager
 
 from prometheus_fastapi_instrumentator import Instrumentator
 from starlette.staticfiles import StaticFiles
-from starlette.templating import Jinja2Templates
 
 from src.addresses.presentation.api import addresses_api_router
 from src.auth.presentation.views import auth_view_router
